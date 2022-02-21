7a4a81f0299f1bac6eeeb5a0b42f056a102ea603
Salihcan Kahya <salihcan.kahya@ibtech.com.tr>
2022-02-21 23:32:41 +0300

    seperated enums for constraints

--- Sources/Swiftlities/Extensions/UIView/Constraints+.swift
+++ Sources/Swiftlities/Extensions/UIView/Constraints+.swift
@@ -8,22 +8,33 @@
 import UIKit
 
 public extension UIView {
-    func add(child view: UIView, pins edges: ViewAnchor...) {
+    
+    func add(child view: UIView, pins edges: PinAnchor...) {
         self.addSubview(view)
         view.translatesAutoresizingMaskIntoConstraints = false
         let constraints = edges.compactMap { $0.createConstraint(on: view, to: self) }
         NSLayoutConstraint.activate(constraints)
     }
     
-    enum ViewAnchor {
+    func activate(pins: PinAnchor..., to parent: UIView) {
+        translatesAutoresizingMaskIntoConstraints = false
+        let constraints = pins.compactMap { $0.createConstraint(on: self, to: parent) }
+        NSLayoutConstraint.activate(constraints)
+    }
+    
+    func activate(dimentions: DimentionAnchor...) {
+        translatesAutoresizingMaskIntoConstraints = false
+        let constraints = dimentions.compactMap { $0.createConstraint(on: self) }
+        NSLayoutConstraint.activate(constraints)
+    }
+    
+    enum PinAnchor {
         case top(ConstraintType = .equalTo())
         case bottom(ConstraintType = .equalTo())
         case leading(ConstraintType = .equalTo())
         case trailing(ConstraintType = .equalTo())
         case centerX(ConstraintType = .equalTo())
         case centerY(ConstraintType = .equalTo())
-        case height(ConstraintType = .equalTo())
-        case width(ConstraintType = .equalTo())
         
         public func createConstraint(on view: UIView, to parent: UIView) -> NSLayoutConstraint {
             switch self {
@@ -36,13 +47,9 @@ public extension UIView {
                 case .trailing(let constaintType):
                     return getConstraint(with: view.trailingAnchor, and: parent.trailingAnchor, constraintType: constaintType)
                 case .centerX(let constaintType):
-                    return getConstraint(with: view.leadingAnchor, and: parent.leadingAnchor, constraintType: constaintType)
+                    return getConstraint(with: view.centerXAnchor, and: parent.centerXAnchor, constraintType: constaintType)
                 case .centerY(let constaintType):
-                    return getConstraint(with: view.leadingAnchor, and: parent.leadingAnchor, constraintType: constaintType)
-                case .height(let constaintType):
-                    return getConstraint(with: view.heightAnchor, constraintType: constaintType)
-                case .width(let constaintType):
-                    return getConstraint(with: view.widthAnchor, constraintType: constaintType)
+                    return getConstraint(with: view.centerYAnchor, and: parent.centerYAnchor, constraintType: constaintType)
             }
         }
         
@@ -58,6 +65,20 @@ public extension UIView {
                     return NSLayoutConstraint()
             }
         }
+    }
+    
+    enum DimentionAnchor {
+        case height(ConstraintType = .equalTo())
+        case width(ConstraintType = .equalTo())
+        
+        public func createConstraint(on view: UIView) -> NSLayoutConstraint {
+            switch self {
+                case .height(let constaintType):
+                    return getConstraint(with: view.heightAnchor, constraintType: constaintType)
+                case .width(let constaintType):
+                    return getConstraint(with: view.widthAnchor, constraintType: constaintType)
+            }
+        }
         
         private func getConstraint(with anchor: NSLayoutDimension, constraintType: ConstraintType) -> NSLayoutConstraint {
             switch constraintType {
@@ -67,7 +88,6 @@ public extension UIView {
                     return NSLayoutConstraint()
             }
         }
-
     }
     
     enum ConstraintType {
