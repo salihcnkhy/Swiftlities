d5148c0745856dc8545113e2332591bb3f634dd3
Salihcan Kahya <salihcan.kahya@ibtech.com.tr>
2022-02-22 18:03:01 +0300

    fillSuperView & centered functions added

--- Sources/Swiftlities/Extensions/UIView/Constraints+.swift
+++ Sources/Swiftlities/Extensions/UIView/Constraints+.swift
@@ -9,20 +9,49 @@ import UIKit
 
 public extension UIView {
     
-    func activate(constraints: [ViewConstraint]) {
+    @discardableResult
+    func activate(constraint: ViewConstraint) -> NSLayoutConstraint {
+        translatesAutoresizingMaskIntoConstraints = false
+        let constraint = constraint.createConstraint(on: self)
+        NSLayoutConstraint.activate(constraints)
+        return constraint
+    }
+    
+    @discardableResult
+    func activate(constraints: [ViewConstraint]) -> [NSLayoutConstraint] {
         translatesAutoresizingMaskIntoConstraints = false
         let constraints = constraints.compactMap { $0.createConstraint(on: self) }
         NSLayoutConstraint.activate(constraints)
+        return constraints
     }
     
     func fill(in view: UIView, with padding: UIEdgeInsets = .zero) {
         view.addSubview(self)
         translatesAutoresizingMaskIntoConstraints = false
+        fillSuperView(with: padding)
+    }
+    
+    func fillSuperView(with padding: UIEdgeInsets = .zero) {
+        guard let view = superview else { return }
         self.activate(constraints: [
             .top(relation: .equal(attribute: .top(ofView: view), padding.top)),
             .bottom(relation: .equal(attribute: .bottom(ofView: view), -padding.bottom)),
             .leading(relation: .equal(attribute: .leading(ofView: view), padding.left)),
             .trailing(relation: .equal(attribute: .trailing(ofView: view), -padding.right))
+        ])
+    }
+    
+    func addCenterXConstraint(with constant: CGFloat = 0) {
+        guard let view = superview else { return }
+        self.activate(constraints: [
+            .centerX(relation: .equal(attribute: .centerX(ofView: view), constant))
+        ])
+    }
+    
+    func addCenterYConstraint(with constant: CGFloat = 0) {
+        guard let view = superview else { return }
+        self.activate(constraints: [
+            .centerY(relation: .equal(attribute: .centerY(ofView: view), constant))
         ])
     }
     
@@ -33,6 +62,8 @@ public extension UIView {
         case trailing(relation: ConstraintRelation)
         case heightTo(relation: ConstraintRelation)
         case widthTo(relation: ConstraintRelation)
+        case centerX(relation: ConstraintRelation)
+        case centerY(relation: ConstraintRelation)
         case height(constantRelation: ConstantConstraintRelation)
         case width(constantRelation: ConstantConstraintRelation)
         
@@ -54,6 +85,10 @@ public extension UIView {
                     return constantRelation.createConstraint(on: view.heightAnchor)
                 case .width(let constantRelation):
                     return constantRelation.createConstraint(on: view.widthAnchor)
+                case .centerX(let constrainRelation):
+                    return constrainRelation.createConstraint(on: view.centerXAnchor)
+                case .centerY(let constrainRelation):
+                    return constrainRelation.createConstraint(on: view.centerYAnchor)
             }
         }
     }
@@ -103,6 +138,8 @@ public extension UIView {
         case trailing(ofView: UIView)
         case height(ofView: UIView)
         case width(ofView: UIView)
+        case centerX(ofView: UIView)
+        case centerY(ofView: UIView)
         
         fileprivate func getAnchor() -> NSLayoutYAxisAnchor {
             switch self {
@@ -110,6 +147,8 @@ public extension UIView {
                     return ofView.topAnchor
                 case .bottom(let ofView):
                     return ofView.bottomAnchor
+                case .centerY(let ofView):
+                    return ofView.centerYAnchor
                 default:
                     fatalError()
             }
@@ -121,6 +160,8 @@ public extension UIView {
                     return ofView.leadingAnchor
                 case .trailing(let ofView):
                     return ofView.trailingAnchor
+                case .centerX(let ofView):
+                    return ofView.centerXAnchor
                 default:
                     fatalError()
             }
