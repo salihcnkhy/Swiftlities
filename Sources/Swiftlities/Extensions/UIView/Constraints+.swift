b7bfb00ec90e2a401456806a5deeaad599625d4a
Salihcan Kahya <salihcan.kahya@ibtech.com.tr>
2022-02-22 17:46:01 +0300

    constraint enums & activate and fillin function

--- Sources/Swiftlities/Extensions/UIView/Constraints+.swift
+++ Sources/Swiftlities/Extensions/UIView/Constraints+.swift
@@ -9,82 +9,134 @@ import UIKit
 
 public extension UIView {
     
-    func add(child view: UIView, constraint anchors: ViewAnchor...) {
-        self.addSubview(view)
-        view.translatesAutoresizingMaskIntoConstraints = false
-        let constraints = anchors.compactMap { $0.createConstraint(on: view) }
-        NSLayoutConstraint.activate(constraints)
-    }
-    
-    func activate(cons: @escaping (UIView) -> [ViewAnchor]) {
-        guard let parentView = self.superview else { return }
-        let viewAnchors = cons(parentView)
+    func activate(constraints: [ViewConstraint]) {
         translatesAutoresizingMaskIntoConstraints = false
-        let constraints = viewAnchors.compactMap { $0.createConstraint(on: self) }
+        let constraints = constraints.compactMap { $0.createConstraint(on: self) }
         NSLayoutConstraint.activate(constraints)
     }
+    
+    func fill(in view: UIView, with padding: UIEdgeInsets = .zero) {
+        view.addSubview(self)
+        translatesAutoresizingMaskIntoConstraints = false
+        self.activate(constraints: [
+            .top(relation: .equal(attribute: .top(ofView: view), padding.top)),
+            .bottom(relation: .equal(attribute: .bottom(ofView: view), -padding.bottom)),
+            .leading(relation: .equal(attribute: .leading(ofView: view), padding.left)),
+            .trailing(relation: .equal(attribute: .trailing(ofView: view), -padding.right))
+        ])
+    }
+    
+    enum ViewConstraint {
+        case top(relation: ConstraintRelation)
+        case bottom(relation: ConstraintRelation)
+        case leading(relation: ConstraintRelation)
+        case trailing(relation: ConstraintRelation)
+        case heightTo(relation: ConstraintRelation)
+        case widthTo(relation: ConstraintRelation)
+        case height(constantRelation: ConstantConstraintRelation)
+        case width(constantRelation: ConstantConstraintRelation)
         
-    enum ViewAnchor {
-        case top(ParentRelatedConstraintType, NSLayoutYAxisAnchor)
-        case bottom(ParentRelatedConstraintType, NSLayoutYAxisAnchor)
-        case leading(ParentRelatedConstraintType, NSLayoutXAxisAnchor)
-        case trailing(ParentRelatedConstraintType, NSLayoutXAxisAnchor)
-        case centerX(ParentRelatedConstraintType, NSLayoutXAxisAnchor)
-        case centerY(ParentRelatedConstraintType, NSLayoutYAxisAnchor)
-        case heightToView(ParentRelatedConstraintType, NSLayoutDimension)
-        case widthToView(ParentRelatedConstraintType, NSLayoutDimension)
-        case height(ConstantReletedConstraintType)
-        case width(ConstantReletedConstraintType)
-        
-        public func createConstraint(on view: UIView) -> NSLayoutConstraint {
+        fileprivate func createConstraint(on view: UIView) -> NSLayoutConstraint {
             switch self {
-                case .top(let constaintType, let parentAnchor):
-                    return getConstraint(with: view.topAnchor, to: parentAnchor, constraintType: constaintType)
-                case .bottom(let constaintType, let parentAnchor):
-                    return getConstraint(with: view.bottomAnchor, to: parentAnchor, constraintType: constaintType)
-                case .leading(let constaintType, let parentAnchor):
-                    return getConstraint(with: view.leadingAnchor, to: parentAnchor, constraintType: constaintType)
-                case .trailing(let constaintType, let parentAnchor):
-                    return getConstraint(with: view.trailingAnchor, to: parentAnchor, constraintType: constaintType)
-                case .centerX(let constaintType, let parentAnchor):
-                    return getConstraint(with: view.centerXAnchor, to: parentAnchor, constraintType: constaintType)
-                case .centerY(let constaintType, let parentAnchor):
-                    return getConstraint(with: view.centerYAnchor, to: parentAnchor, constraintType: constaintType)
-                case .heightToView(let constaintType, let parentAnchor):
-                    return getConstraint(with: view.heightAnchor, to: parentAnchor, constraintType: constaintType)
-                case .widthToView(let constaintType, let parentAnchor):
-                    return getConstraint(with: view.widthAnchor, to: parentAnchor, constraintType: constaintType)
-                case .height(let constaintType):
-                    return getConstraint(with: view.heightAnchor, constraintType: constaintType)
-                case .width(let constaintType):
-                    return getConstraint(with: view.widthAnchor, constraintType: constaintType)
- 
-            }
-        }
-        
-        private func getConstraint<AnchorType>(with anchor: NSLayoutAnchor<AnchorType>, to parent: NSLayoutAnchor<AnchorType>, constraintType: ParentRelatedConstraintType) -> NSLayoutConstraint {
-            switch constraintType {
-                case .equalTo(let padding), .greaderThan(let padding), .lessThan(let padding):
-                   return anchor.constraint(equalTo: parent, constant: padding)
-            }
-        }
-        
-        private func getConstraint(with anchor: NSLayoutDimension, constraintType: ConstantReletedConstraintType) -> NSLayoutConstraint {
-            switch constraintType {
-                case .equalToConstant(let padding):
-                    return anchor.constraint(equalToConstant: padding)
+                case .top(let constrainRelation):
+                    return constrainRelation.createConstraint(on: view.topAnchor)
+                case .bottom(let constrainRelation):
+                    return constrainRelation.createConstraint(on: view.bottomAnchor)
+                case .leading(let constrainRelation):
+                    return constrainRelation.createConstraint(on: view.leadingAnchor)
+                case .trailing(let constrainRelation):
+                    return constrainRelation.createConstraint(on: view.trailingAnchor)
+                case .heightTo(let constrainRelation):
+                    return constrainRelation.createConstraint(on: view.heightAnchor)
+                case .widthTo(let constrainRelation):
+                    return constrainRelation.createConstraint(on: view.widthAnchor)
+                case .height(let constantRelation):
+                    return constantRelation.createConstraint(on: view.heightAnchor)
+                case .width(let constantRelation):
+                    return constantRelation.createConstraint(on: view.widthAnchor)
             }
         }
     }
     
-    enum ParentRelatedConstraintType {
-        case equalTo(CGFloat = 0)
-        case greaderThan(CGFloat = 0)
-        case lessThan(CGFloat = 0)
+    enum ConstantConstraintRelation {
+        case equalConstant(CGFloat = 0)
+        
+        fileprivate func createConstraint(on anchor: NSLayoutDimension) -> NSLayoutConstraint {
+            switch self {
+                case .equalConstant(let cGFloat):
+                    return anchor.constraint(equalToConstant: cGFloat)
+            }
+        }
     }
     
-    enum ConstantReletedConstraintType {
-        case equalToConstant(CGFloat = 0)
+    enum ConstraintRelation {
+        case equal(attribute: ConstraintRelationAttribute, CGFloat = 0)
+        case greaterThan(attribute: ConstraintRelationAttribute, CGFloat = 0)
+        case lessThan(attribute: ConstraintRelationAttribute, CGFloat = 0)
+        
+        fileprivate func createConstraint(on anchor: NSLayoutYAxisAnchor) -> NSLayoutConstraint {
+            switch self {
+                case .equal(let constraintRelationAttribute, let cGFloat), .greaterThan(let constraintRelationAttribute, let cGFloat), .lessThan(let constraintRelationAttribute, let cGFloat):
+                    return anchor.constraint(equalTo: constraintRelationAttribute.getAnchor(), constant: cGFloat)
+            }
+        }
+        
+        fileprivate func createConstraint(on anchor: NSLayoutXAxisAnchor) -> NSLayoutConstraint {
+            switch self {
+                case .equal(let constraintRelationAttribute, let cGFloat), .greaterThan(let constraintRelationAttribute, let cGFloat), .lessThan(let constraintRelationAttribute, let cGFloat):
+                    return anchor.constraint(equalTo: constraintRelationAttribute.getAnchor(), constant: cGFloat)
+            }
+        }
+        
+        fileprivate func createConstraint(on anchor: NSLayoutDimension) -> NSLayoutConstraint {
+            switch self {
+                case .equal(let constraintRelationAttribute, let cGFloat), .greaterThan(let constraintRelationAttribute, let cGFloat), .lessThan(let constraintRelationAttribute, let cGFloat):
+                    return anchor.constraint(equalTo: constraintRelationAttribute.getAnchor(), constant: cGFloat)
+            }
+        }
     }
+    
+    enum ConstraintRelationAttribute {
+        case top(ofView: UIView)
+        case bottom(ofView: UIView)
+        case leading(ofView: UIView)
+        case trailing(ofView: UIView)
+        case height(ofView: UIView)
+        case width(ofView: UIView)
+        
+        fileprivate func getAnchor() -> NSLayoutYAxisAnchor {
+            switch self {
+                case .top(let ofView):
+                    return ofView.topAnchor
+                case .bottom(let ofView):
+                    return ofView.bottomAnchor
+                default:
+                    fatalError()
+            }
+        }
+        
+        fileprivate func getAnchor() -> NSLayoutXAxisAnchor {
+            switch self {
+                case .leading(let ofView):
+                    return ofView.leadingAnchor
+                case .trailing(let ofView):
+                    return ofView.trailingAnchor
+                default:
+                    fatalError()
+            }
+        }
+        
+        fileprivate func getAnchor() -> NSLayoutDimension {
+            switch self {
+                case .height(let ofView):
+                    return ofView.heightAnchor
+                case .width(let ofView):
+                    return ofView.widthAnchor
+                default:
+                    fatalError()
+            }
+        }
+    }
+    
 }
 
